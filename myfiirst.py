c96874676a10a40faed15291ffe6fc3b9630305f
Silvana <das.eis@gmail.com>
2017-10-12 08:39:03 +0200

    test

---
+++ myfiirst.py
@@ -0,0 +1,33 @@
+"""fjdskd."""
+
+
+def myclass():
+    """fjdskd."""
+    print("hello world")
+
+
+def myname(arg1):
+    """fjdskd."""
+    pass
+
+
+def mNNyname2(arg2):
+    """fjdskd."""
+    print("x")
+
+
+def product(x, y):
+    """fjdskd."""
+    return x*y
+
+
+def main():
+    """fjdskd."""
+    return product(5, 5)
+
+
+if __name__ == '__main__':
+    print("running")
+    print(main())
+
+# print("test")
---
+++ mytest.py
@@ -0,0 +1,22 @@
+"""fjdskd."""
+from myfiirst import product
+
+
+def test_myclass_test():
+    """fjdskd."""
+    assert 5*5 is 25
+
+
+def test_myname_test():
+    """fjdskd."""
+    assert product(5, 5) is 25
+
+
+def test_mNNyname2_test():
+    """fjdskd."""
+    assert product(5, 5) is 25
+
+
+def test_product_test():
+    """fjdskd."""
+    assert product(5, 5) is 25
